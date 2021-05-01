403c0913948e2255dcd9149e9f159253218e7133
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-05-01 16:43:59 -0300

    criado o controller de atualização do cliente

--- PETSHOP/src/controllers/ClienteController.ts
+++ PETSHOP/src/controllers/ClienteController.ts
@@ -21,15 +21,16 @@ class ClienteController{
 
     async listarPorId(request: Request, response: Response) {
         try {
-        const { id } = request.params;
-        const cliente = await ClienteSchema.find({ _id: id });
-        if (cliente != null){
-            response.status(200).json({ data: cliente, error: false, msg: "Cliente encontrado!", });
-        }
-            response.status(400).json({ data: cliente, error: false, msg: "Cliente não encontrado!", });
-        } catch (error) {
-            response.status(400).json({ data: error, error: true, msg: "Formato de id não válido!", });
-        }   
+            const { id } = request.params;
+            const cliente = await ClienteSchema.find({ _id: id });
+
+            if (cliente != null){
+                response.status(200).json({ data: cliente, error: false, msg: "Cliente encontrado!", });
+            }
+                response.status(400).json({ data: cliente, error: false, msg: "Cliente não encontrado!", });
+            } catch (error) {
+                response.status(400).json({ data: error, error: true, msg: "Formato de id não válido!", });
+            }   
      }
 
      async excluir(request: Request, response: Response){
@@ -48,12 +49,62 @@ class ClienteController{
 
     async cadastrar(request: Request, response: Response) {
         try {
-            //await para dar uma pausa antes de ir para o próximo processo, por isso usado o async, usados em operações que envolvem o banco de dados
+            //await para dar uma pausa antes de ir para o próximo processo, 
+            //por isso usado o async, usados em operações que envolvem o banco de dados
             const cliente = await ClienteSchema.create(request.body);
             response.status(201).json({ data: cliente, error: false, msg: "Cliente cadastrado com sucesso!", });
         } catch (error) {
             response.status(400).json({ data: error, error: true, msg: "Não foi possível cadastrar o cliente.", });
         }   
+    }
+    async alterar(request: Request, response: Response) {
+        try {
+            const { id } = request.params;
+            const { nome, cpf, telefone, endereco } = request.body;
+
+            const cliente = await ClienteSchema.findByIdAndUpdate(
+                {
+                    _id: id
+                },
+                {
+                    nome: nome,
+                    cpf: cpf,
+                    telefone: telefone,
+                    endereco: endereco,
+                },
+                {
+                    new:true,
+                    useFindAndModify: false,
+                } 
+            );
+
+            if(cliente != null){
+                response.status(200).json(
+                    {
+                        data: cliente,
+                        error:false,
+                        msg:"O cliente foi atualizado!"
+                    }
+                );
+            }
+            else{
+                response.status(404).json(
+                    {
+                        data: "Falha",
+                        error:true,
+                        msg:"O cliente não foi encontrado!"
+                    }
+                );
+            }
+        } catch (error) {
+            response.status(404).json(
+                {
+                    data: error,
+                    error:true,
+                    msg:"O id informado não é válido!"
+                }
+            );
+        }
     }
 }
 export { ClienteController };
