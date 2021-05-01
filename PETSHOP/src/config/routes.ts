68a649324af3fa86bd67d21566bac0812a7e681f
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-05-01 16:43:16 -0300

    criado a rota de atualização do cliente

--- PETSHOP/src/config/routes.ts
+++ PETSHOP/src/config/routes.ts
@@ -9,6 +9,6 @@ router.get("/cliente/listar/:id", clienteController.listarPorId);
 //router.get("/cliente/listarPorCpf/", clienteController.listarPorCpf);
 router.post("/cliente/cadastrar", clienteController.cadastrar);
 router.delete("/cliente/excluir/:id", clienteController.excluir);
-
+router.patch("/cliente/alterar/:id",clienteController.alterar);
 
 export { router };
