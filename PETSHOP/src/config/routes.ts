fa47965c03d8794c6c4a7aa6bc857484d89440ba
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-05-01 20:22:28 -0300

    criado as rotas

--- PETSHOP/src/config/routes.ts
+++ PETSHOP/src/config/routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
+import { AnimalController } from "../controllers/AnimalController";
+import { AtendimentoController } from "../controllers/AtendimentoController";
 import { ClienteController } from "../controllers/ClienteController";
 import { FuncionarioController } from "../controllers/FuncionarioController";
+import { ProcedimentoController } from "../controllers/ProcedimentoController";
 
 const router = Router();
+
 const clienteController = new ClienteController();
 const funcionarioController = new FuncionarioController();
+const animalController = new AnimalController();
+const atendimentoController = new AtendimentoController();
+const procedimentoController =  new ProcedimentoController();
 
 //rotas para clientes
 router.get("/cliente/listar", clienteController.listar);
@@ -13,11 +20,22 @@ router.get("/cliente/listar/:id", clienteController.listarPorId);
 router.post("/cliente/cadastrar", clienteController.cadastrar);
 router.delete("/cliente/excluir/:id", clienteController.excluir);
 router.patch("/cliente/alterar/:id",clienteController.alterar);
+
 //rotas para funcionários
 router.post("/funcionario/cadastrar", funcionarioController.cadastrar);
 router.patch("/funcionario/alterar/:id/:estado",funcionarioController.alterar);
 
+//rotas para animal
+router.post("/animal/cadastrar", animalController.cadastrar);
+router.patch("/funcionario/alterar/:id",animalController.alterar);
 
+//rotas para atendimento
+router.post("/atendimento/cadastrar", atendimentoController.cadastrar);
+router.patch("/atendimento/alterar/:id",atendimentoController.alterar);
+
+//rotas para procedimento
+router.post("/atendimento/cadastrar", atendimentoController.cadastrar);
+router.patch("/atendimento/alterar/:id",atendimentoController.alterar);
 
 
 export { router };
